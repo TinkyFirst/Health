e1bd5bcecc472ea826a8b9621d8f949e771a11d5
TinkyFirst <53491546+TinkyFirst@users.noreply.github.com>
2021-08-15 21:42:55 +0300

    fix container

--- script/slider.js
+++ script/slider.js
@@ -1,5 +1,5 @@
-let left = document.getElementById('left');
-let right = document.getElementById('right');
+let left = document.querySelector('svg.arrow-left');
+let right = document.querySelector('svg.arrow-right');
 let text = document.getElementById('text');
 let img = document.getElementById('img');
 let days = document.querySelectorAll('.Day');
@@ -20,43 +20,86 @@ let day_number = [
 ]
 let days_content = [
     [
-        'Прилет (время зависит от выбранного рейса) Трансфер из аэропорта в отель Заселение в отеле на выбор (3, 4, 5 звезд) в Азиатской части Стамбула, район Кадикей (Мода) рядом с набережной Мраморного моря Рекомендуемый маршрут для самостоятельной прогулки и посещения ресторана Связь с организатором в мессенджере',
-        '/assets/img/img1.png'
+        // сделал отдельный массив для ли
+        [`<li>Прилет (время зависит от выбранного рейса)</li>`,
+            `<li>Трансфер из аэропорта в отель</li>`,
+            `<li>Заселение в отеле на выбор (3, 4, 5 звезд) в Азиатской части Стамбула, район Кадикей (Мода) рядом с набережной Мраморного моря</li>`,
+            `<li>Рекомендуемый маршрут для самостоятельной прогулки и посещения ресторана</li>`,
+            `<li>Связь с организатором в мессенджере</li>`
+        ],
+        `/assets/img/img1.png`
     ],
     [
-        'Трансфер из отеля в клинику + прохождение медосмотра (чек-ап), время прохождения с 9.30 до 15.00, сопровождение русско-, англо-, украино- говорящего переводчика + рекомендуемый маршрут для самостоятельной прогулки и посещения ресторанов. 📱связь с организатором в мессенджере или в индивидуальном порядке – сопровождение 💃чек-ап включает в себя 1 вид обследования: расширенный женский 🧑‍⚕️отдельно можно заказать консультации других нужных специалистов и другие виды обследований',
-        '/assets/img/img2.png'
+        [`<li>Трансфер из отеля в клинику + прохождение медосмотра (чек-ап), время прохождения с 9.30 до 15.00, сопровождение русско-, англо-, украино- говорящего переводчика + рекомендуемый маршрут для самостоятельной прогулки и посещения ресторанов.</li>`,
+            `<li>📱связь с организатором в мессенджере или в индивидуальном порядке – сопровождение 💃чек-ап включает в себя 1 вид обследования: расширенный женский 🧑‍⚕️отдельно можно заказать консультации других нужных специалистов и другие виды обследований</li>`
+        ],
+        `/assets/img/img2.png`
     ],
     [
-        '🎁День шоппинг-релакса с гидом – немного истории, неспешные прогулки по старинным улицам Стамбула, бокал крафтового турецкого вина. Вы увидите Стамбульские Елисейские поля, где расположены бутики премиальных турецких дизайнеров. 📱сопровождение организатора',
-        '/assets/img/img3.png'
+        [`<li>🎁День шоппинг-релакса с гидом – немного истории, неспешные прогулки по старинным улицам Стамбула, бокал крафтового турецкого вина. Вы увидите Стамбульские Елисейские поля, где расположены бутики премиальных турецких дизайнеров.</li>`,
+            `<li>📱сопровождение организатора</li>`
+        ],
+        `/assets/img/img3.png`
     ],
     [
-        '🛫трансфер из отеля в аэропорт+отлет 📱связь с организатором в мессенджере',
-        '/assets/img/img4.png'
+        [`<li>🛫трансфер из отеля в аэропорт+отлет 📱связь с организатором в мессенджере</li>`],
+        `/assets/img/img4.png`
     ]
 ]
+left.querySelector('path').classList.add('svh-arrow-noActive');
 
 let current_slide = 0;
-
-for (let i = 0; i < days.length; i++){
-    days[i].addEventListener('click', function (){
+for (let i = 0; i < days.length; i++) {
+    // добавил приначале загрузки странице
+    right.addEventListener('mouseover',() =>{
+        if (i== 0) {
+            right.querySelector('path').classList.add('svh-arrow-color');
+        }
+    });
+    days[i].addEventListener('click', function () {
+        // Удаляю класс для картинки
+        img.classList.remove('check-up-img');
         days[current_slide].classList.remove('selected')
         current_slide = i;
         days[current_slide].classList.add('selected')
         day.innerHTML = day_number[current_slide][0]
-        text.innerHTML = days_content[current_slide][0]
-        img.src = days_content[current_slide][1]
+        // Удаляю класс для всех дней там День 1,День 2 и тд
+        day.classList.remove("slider-name");
+        text.querySelectorAll('li').forEach((item, a) => {
+            // удаляю из HTML все ли
+            item.remove();
+            if (a == 0) {
+                // нахожу нужный массив
+                mas_text = days_content[i][0];
+                // перебираю его
+                mas_text.forEach(item => {
+                    // вставляю в HTML нужные ли из массива
+                    text.insertAdjacentHTML('beforeend', item);
+                });
+            }
 
-        if (current_slide === 3){
-            right.src = '/assets/img/arrow-black-right.svg';
-        }else {
-            right.src = '/assets/img/arrow-right.svg';
+        });
+        img.src = days_content[current_slide][1]
+        // добавляю  класс для картинки и для текста что бы анимация для всех появлялась
+        setInterval(() => {
+            img.classList.add('check-up-img');
+            day.classList.add("slider-name");
+        }, 0);
+        // добавляю классы ждя стрелок и удаляю их
+        if (current_slide !== 3) {
+            right.querySelector('path').classList.remove('svh-arrow-noActive');
+            right.querySelector('path').classList.add('svh-arrow-color');
+        } else {
+            right.querySelector('path').classList.remove('svh-arrow-color');
+            right.querySelector('path').classList.add('svh-arrow-noActive');
+
         }
-        if (current_slide === 0){
-            left.src = '/assets/img/arrow-left.svg';
-        }else {
-            left.src = '/assets/img/arrow-black-left.svg';
+        if (current_slide === 0) {
+            left.querySelector('path').classList.add('svh-arrow-noActive');
+            left.querySelector('path').classList.remove('svh-arrow-color');
+        } else {
+            left.querySelector('path').classList.add('svh-arrow-color');
+            left.querySelector('path').classList.remove('svh-arrow-noActive');
         }
     })
 }
@@ -64,37 +107,70 @@ for (let i = 0; i < days.length; i++){
 
 right.onclick = function () {
     if (current_slide !== 3) {
+        img.classList.remove('check-up-img');
         days[current_slide].classList.remove('selected')
         current_slide++
         days[current_slide].classList.add('selected')
+        day.classList.remove("slider-name");
         day.innerHTML = day_number[current_slide][0]
-        text.innerHTML = days_content[current_slide][0]
-        img.src = days_content[current_slide][1]
+        text.querySelectorAll('li').forEach((item, a) => {
+            item.remove();
+            if (a == 0) {
+                mas_text = days_content[current_slide][0];
+                mas_text.forEach(item => {
+                    text.insertAdjacentHTML('beforeend', item);
+                });
+            }
 
-        if (current_slide === 3){
-            right.src = '/assets/img/arrow-black-right.svg';
-        }else {
-            right.src = '/assets/img/arrow-right.svg';
-            left.src = '/assets/img/arrow-black-left.svg';
-        }
+        });
+        img.src = days_content[current_slide][1]
+        setInterval(() => {
+            img.classList.add('check-up-img');
+            day.classList.add("slider-name");
+        }, 0);
+
+    }
+    if (current_slide !== 3) {
+        left.querySelector('path').classList.add('svh-arrow-color');
+        left.querySelector('path').classList.remove('svh-arrow-noActive');
+        right.querySelector('path').classList.remove('svh-arrow-noActive');
+        right.querySelector('path').classList.add('svh-arrow-color');
+    } else {
+        right.querySelector('path').classList.remove('svh-arrow-color');
+        right.querySelector('path').classList.add('svh-arrow-noActive');
     }
 }
 
 left.onclick = function () {
     if (current_slide !== 0) {
+        img.classList.remove('check-up-img');
         days[current_slide].classList.remove('selected')
         current_slide--
         days[current_slide].classList.add('selected')
+        day.classList.remove("slider-name");
         day.innerHTML = day_number[current_slide][0]
-        text.innerHTML = days_content[current_slide][0]
+        text.querySelectorAll('li').forEach((item, a) => {
+            item.remove();
+            if (a == 0) {
+                mas_text = days_content[current_slide][0];
+                mas_text.forEach(item => {
+                    text.insertAdjacentHTML('beforeend', item);
+                });
+            }
+        });
         img.src = days_content[current_slide][1]
-
-        if (current_slide === 0){
-            left.src = '/assets/img/arrow-left.svg';
-            right.src = '/assets/img/arrow-right.svg';
-        }else {
-            left.src = '/assets/img/arrow-black-left.svg';
+        setInterval(() => {
+            img.classList.add('check-up-img');
+            day.classList.add("slider-name");
+        }, 0);
+        if (current_slide !== 0) {
+            right.querySelector('path').classList.remove('svh-arrow-noActive');
+            right.querySelector('path').classList.add('svh-arrow-color');
+            left.querySelector('path').classList.add('svh-arrow-color');
+            left.querySelector('path').classList.remove('svh-arrow-noActive');
+        } else {
+            left.querySelector('path').classList.add('svh-arrow-noActive');
+            left.querySelector('path').classList.remove('svh-arrow-color');
         }
     }
 }
-
