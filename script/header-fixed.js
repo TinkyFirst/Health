3171a4acae00353cf617d8fc2660fe54f548b78d
TinkyFirst <53491546+TinkyFirst@users.noreply.github.com>
2021-08-16 21:41:08 +0300

    fix header fix

---
+++ script/header-fixed.js
@@ -0,0 +1,25 @@
+window.onscroll = function showHeader() {
+    var header = document.querySelector('.headerMenu');
+    var headerNumber = document.querySelector('.phone');
+    var headerIcon = document.querySelector('.phone-icon');
+    var headerNumber = document.querySelector('.phone');
+    var BuyNumber = document.querySelector('.buy-number');
+    var BtnConsulting = document.querySelector('.btn-consulting-mobile')
+
+    if (window.pageYOffset > 200) {
+        header.classList.add('headerMenu-fixed, header-text-fix');
+        headerNumber.classList.add('phone-fixed');
+        headerIcon.classList.add('phone-icon-fix');
+        BuyNumber.classList.add('buy-number-fixed');
+        BtnConsulting.classList.add('btn-consulting-mobile-fixed');
+
+    } else {
+        header.classList.add('headerMenu-fixed');
+        headerNumber.classList.add('phone-fixed');
+        headerIcon.classList.add('phone-icon-fix');
+        BuyNumber.classList.add('buy-number-fixed');
+        BtnConsulting.classList.add('btn-consulting-mobile-fixed');
+
+    }
+}
+
