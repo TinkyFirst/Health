f015d2b0e407c8c9dba6b166cdf4d2a04462f923
TinkyFirst <53491546+TinkyFirst@users.noreply.github.com>
2021-08-19 10:54:11 +0300

    fix header fix

--- script/header-fixed.js
+++ script/header-fixed.js
@@ -6,22 +6,25 @@ window.onscroll = function showHeader() {
     var BtnConsulting = document.querySelector('.btn-consulting-mobile');
     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     var BtnConsultingPc = document.querySelector('.btn-consulting-pc');
+    var PhoneHeader = document.querySelector('.phone-header')
 
-    if (window.pageYOffset > 200) {
+    if (window.pageYOffset > 1000) {
         header.classList.add('headerMenu-fixed');
         headerNumber.classList.add('phone-fixed');
         headerIcon.classList.add('phone-icon-fix');
         BuyNumber.classList.add('buy-number-fixed');
         BtnConsulting.classList.add('btn-consulting-mobile-fixed');
         BtnConsultingPc.classList.add('btn-consulting-pc-fixed');
+        PhoneHeader.classList.add('phone-header-fixed');
+
 
     } else {
-        header.classList.add('headerMenu-fixed');
-        headerNumber.classList.add('phone-fixed');
-        headerIcon.classList.add('phone-icon-fix');
-        BuyNumber.classList.add('buy-number-fixed');
-        BtnConsulting.classList.add('btn-consulting-mobile-fixed');
-        BtnConsultingPc.classList.add('btn-consulting-pc-fixed');
+        header.classList.remove('headerMenu-fixed');
+        headerNumber.classList.remove('phone-fixed');
+        headerIcon.classList.remove('phone-icon-fix');
+        BuyNumber.classList.remove('buy-number-fixed');
+        BtnConsulting.classList.remove('btn-consulting-mobile-fixed');
+        BtnConsultingPc.classList.remove('btn-consulting-pc-fixed');
 
     }
     console.log(scrollTop)
